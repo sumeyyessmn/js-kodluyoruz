df421086b5eefe1a152c4b7d7d2016b2ea162026
sumeyyessmn <sumeyye.ssmn@gmail.com>
2023-05-24 21:55:50 +0300

    formcalismasieklendi

---
+++ form-bolum-sonu/formlarla-calismak.js
@@ -0,0 +1,8 @@
+//FORMLARLA ÇALIŞMAK: Bölüm Sonu Egzersizi
+
+/* 1-Formu Seç
+2.İnput bilgisini UL içine ekle
+3. Form İçindeki bilgiyi sıfırla
+4. Eğer forma bilgi girilmezse kullanıcıyı uyar */
+
+
